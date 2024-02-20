76ec81f617f3802a1c9993b5069e7a052ab45d50
nagul_s <nagul.s@itprofound.com>
2024-02-20 15:53:32 +0530

    first commit

---
+++ src/main/java/payrollweb/itprofound/PayrollwebBackendApplication.java
@@ -0,0 +1,13 @@
+package payrollweb.itprofound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollwebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollwebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/payrollweb/itprofound/PayrollwebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package payrollweb.itprofound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollwebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
